6d79d2ec56cb354ebce995733d3a7460820a64de
Disana94 <127531324+Disana94@users.noreply.github.com>
2025-09-26 19:24:14 +0300

    Update hash.py

--- hash.py
+++ hash.py
@@ -1,4 +1,5 @@
-# Хэш-таблица в Python реализована как dict
-hash_table = {"apple": 3, "banana": 5}
+from collections import dict  
+
+hash_table = dict({"apple": 3, "banana": 5})
 hash_table["orange"] = 7
 print(hash_table["banana"])  # 5
